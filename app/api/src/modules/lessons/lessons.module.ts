406e8ce35e2d63c9999623f0dc6be5e4e2690fd0
godkor200 <godkor200@gmail.com>
2023-09-02 08:33:19 +0900

    refacter : 모듈에 id_token 추가

--- app/api/src/modules/lessons/lessons.module.ts
+++ app/api/src/modules/lessons/lessons.module.ts
@@ -1,30 +1,73 @@
 import { Module, Provider } from '@nestjs/common';
-import { GetLessonInfoHttpController } from './queries/Get-lesson-info/get-lesson-info.http.controller';
+import { GetLessonAvailableInfoHttpController } from './queries/Get-lesson-available-info/get-lesson-available-info.http.controller';
 import { EntityModule } from './database/entitity/entity.module';
 import {
-  GET_COACHES_INFO,
+  COACH_REPOSITORY,
+  CUSTOMERS_REPOSITORY,
+  DEL_LESSON,
+  GET_LESSON_INFO,
   GET_LESSON_INFO_LIST,
-  GET_LESSON_LIST,
+  LESSON_REPOSITORY,
+  POST_LESSON,
+  PUT_LESSON,
 } from './constants/lesson.di-token';
-import { GetLessonInfoQueryHandler } from './queries/Get-lesson-info/get-lesson-info.query-handler';
+import { GetLessonAvailableInfoQueryHandler } from './queries/Get-lesson-available-info/get-lesson-available-info.query-handler';
 import { CoachesRepository } from './database/repository/coaches.repository';
 import { LessonRepository } from './database/repository/lesson.repository';
+import { ReqLessonHttpController } from './command/req-lesson/req-lesson.http.controller';
+import { ReqLessonCommandHandler } from './command/req-lesson/req-lesson.command-handler';
+import { CustomersRepository } from './database/repository/customers.repository';
+import { GetLessonInfoQueryHandler } from './queries/Get-lesson-info/get-lesson-info.query-handler';
+import { GetLessonInfoHttpController } from './queries/Get-lesson-info/get-lesson-info.http.controller';
+import { PutLessonInfoHttpController } from './command/put-lesson-info/put-lesson-info.http.controller';
+import { PutLessonInfoCommandHandler } from './command/put-lesson-info/put-lesson-info.command-handler';
+import { DeleteLessonCommandHandler } from './command/delete-lesson/delete-lesson.command-handler';
+import { DeleteLessonHttpController } from './command/delete-lesson/delete-lesson.http.controller';
 
 const queryHandlers: Provider[] = [
   {
     provide: GET_LESSON_INFO_LIST,
+    useClass: GetLessonAvailableInfoQueryHandler,
+  },
+  {
+    provide: GET_LESSON_INFO,
     useClass: GetLessonInfoQueryHandler,
   },
 ];
 
+const commandHandlers: Provider[] = [
+  {
+    provide: POST_LESSON,
+    useClass: ReqLessonCommandHandler,
+  },
+  {
+    provide: PUT_LESSON,
+    useClass: PutLessonInfoCommandHandler,
+  },
+  {
+    provide: DEL_LESSON,
+    useClass: DeleteLessonCommandHandler,
+  },
+];
+
 const repositories: Provider[] = [
-  { provide: GET_COACHES_INFO, useClass: CoachesRepository },
-  { provide: GET_LESSON_LIST, useClass: LessonRepository },
+  { provide: COACH_REPOSITORY, useClass: CoachesRepository },
+  { provide: LESSON_REPOSITORY, useClass: LessonRepository },
+  {
+    provide: CUSTOMERS_REPOSITORY,
+    useClass: CustomersRepository,
+  },
 ];
 
 @Module({
   imports: [EntityModule],
-  controllers: [GetLessonInfoHttpController],
-  providers: [...queryHandlers, ...repositories],
+  controllers: [
+    GetLessonAvailableInfoHttpController,
+    PutLessonInfoHttpController,
+    GetLessonInfoHttpController,
+    ReqLessonHttpController,
+    DeleteLessonHttpController,
+  ],
+  providers: [...queryHandlers, ...repositories, ...commandHandlers],
 })
 export class LessonsModule {}
