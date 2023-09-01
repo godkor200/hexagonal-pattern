8c307f8c437e6a6242d61023b1b0751a5ec57106
godkor200 <godkor200@gmail.com>
2023-09-02 08:32:29 +0900

    refacter :기본 쿼리 이셉션추가

--- app/api/src/common/db/sql-repository.base.ts
+++ app/api/src/common/db/sql-repository.base.ts
@@ -4,40 +4,77 @@ import {
   RepositoryPort,
   updateObject,
 } from './sql-repository.interface';
+import { HttpException, HttpStatus } from '@nestjs/common';
 export abstract class SqlRepositoryBase<E> implements RepositoryPort<E> {
   constructor(private dataSource: DataSource) {}
   protected abstract tableName: string;
   protected abstract repository: Repository<E>;
 
   async delete(id: string): Promise<boolean> {
-    const res = await this.repository
-      .createQueryBuilder(this.tableName)
-      .delete()
-      .from(this.tableName)
-      .where({ id })
-      .execute();
-    return res.raw > 0;
+    try {
+      const res = await this.repository
+        .createQueryBuilder(this.tableName)
+        .delete()
+        .from(this.tableName)
+        .where({ id })
+        .execute();
+      return res.raw > 0;
+    } catch (err) {
+      throw new HttpException(
+        {
+          message: err,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async findAll(): Promise<E[]> {
-    return await this.repository.find();
+    try {
+      return await this.repository.find();
+    } catch (err) {
+      throw new HttpException(
+        {
+          message: err,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  async findOneById(id: string): Promise<E> {
-    return await this.repository
-      .createQueryBuilder(this.tableName)
-      .where({ id: id })
-      .getOne();
+  async findOneById(id: string | number): Promise<E> {
+    try {
+      return await this.repository
+        .createQueryBuilder(this.tableName)
+        .where({ id: id })
+        .getOne();
+    } catch (err) {
+      throw new HttpException(
+        {
+          message: err,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async insert(entity: E): Promise<IResDto> {
-    const res = await this.repository
-      .createQueryBuilder(this.tableName)
-      .insert()
-      .into(this.tableName)
-      .values(entity)
-      .execute();
-    return { success: res.raw > 0 };
+    try {
+      const res = await this.repository
+        .createQueryBuilder(this.tableName)
+        .insert()
+        .into(this.tableName)
+        .values(entity)
+        .execute();
+      return { success: res.raw > 0 };
+    } catch (err) {
+      throw new HttpException(
+        {
+          message: err,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async transaction<T>(handler: () => Promise<T>): Promise<void | T> {
@@ -56,12 +93,21 @@ export abstract class SqlRepositoryBase<E> implements RepositoryPort<E> {
   }
 
   async updateOne(params: updateObject): Promise<IResDto> {
-    const res = await this.repository
-      .createQueryBuilder()
-      .update(this.tableName)
-      .set(params)
-      .where({ id: params.id })
-      .execute();
-    return { success: res.raw > 0 };
+    try {
+      const res = await this.repository
+        .createQueryBuilder()
+        .update(this.tableName)
+        .set(params)
+        .where({ id: params.id })
+        .execute();
+      return { success: res.affected > 0 };
+    } catch (err) {
+      throw new HttpException(
+        {
+          message: err.sqlMessage,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
